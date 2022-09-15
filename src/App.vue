8133ad1c2d91889533051cdc04e4e42b4b469fd7
ziyuan.wang <ziyuan.wang@accenture.com>
2022-09-15 20:40:32 +0900

    Revert "edit app.vue"
    This reverts commit 90757188849225a29e358e70ee5ac1eecb5472b1.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,14 @@
+<template>
+  <div>Hello {{ value }}</div>
+</template>
+
 <script>
 export default {
+  name: "App",
   data() {
     return {
-      text: 'Edit me',
-      checked: true,
-      checkedNames: ['Miyazawa'],
-      picked: 'One',
-      selected: 'A',
-      multiSelected: ['A']
-    }
+      value: "World"
+    };
   }
-}
+};
 </script>
-
-<template>
-  <h2>School Service Test</h2>
-  <input v-model="text"> {{ text }}
-
-
-  <h2>check!check!</h2>
-  <input type="checkbox" id="Miyazawa" value="Miyazawa" v-model="checkedNames">
-  <label for="miyazawa">Miyazawa</label>
-  <input type="checkbox" id="Ziyuan" value="Ziyuan" v-model="checkedNames">
-  <label for="ziyuan">Ziyuan</label>
-  <input type="checkbox" id="SatoT" value="SatoT" v-model="checkedNames">
-  <label for="sato">SatoT</label>
-  <p>Checked names: <pre>{{ checkedNames }}</pre></p>
-
-</template>

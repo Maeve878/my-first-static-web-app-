f5ae3b0604b769c2f3794a419e4f8daea8fa2a59
ziyuan.wang <ziyuan.wang@accenture.com>
2022-09-15 20:51:53 +0900

    edit file

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div>Hello {{ value }}</div>
+  <h2>出席チェック</h2>
+  <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
+  <label for="jack">Miyazawa</label>
+  <input type="checkbox" id="john" value="John" v-model="checkedNames">
+  <label for="john">Ziyuan</label>
+  <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
+  <label for="mike">SatoT</label>
 </template>
 
 <script>
@@ -7,7 +14,7 @@ export default {
   name: "App",
   data() {
     return {
-      value: "World"
+      value: "School Service Test!!!!!!!"
     };
   }
 };
